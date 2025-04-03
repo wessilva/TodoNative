de88d105cdd1acc473a9e916c117d6df3b908629
wessilva <silveirawesley.1996gmail.com>
2025-04-03 18:12:22 -0300

    feat: adiciona funcionalidades de remoção e alternância de tarefas na lista

--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -2,13 +2,30 @@ import { FlatList, Text, TouchableOpacity, View } from "react-native";
 import { Task } from "../../types/"
 import { AntDesign } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { TaskEmpty } from "../TaskEmpty";
+import { useState } from "react";
 
 interface TasklistProps {
     tasks: Task[];
+    onToggleTask: (id: string) => void;
+    onRemoveTask: (id: string) => void;
 }
 
-export const TaskList = ({ tasks }: TasklistProps) => {
+export const TaskList = ({ tasks, onToggleTask, onRemoveTask }: TasklistProps) => {
+
+    const [pressedId, setPressedId] = useState<string | null>(null);
+    const [removingId, setRemovingId] = useState<string | null>(null)
+
+    const handleRemoveWithDelay = (id: string) => {
+
+        setRemovingId(id);
+
+        setTimeout(() => {
+            onRemoveTask(id);
+            setRemovingId(null);
+        }, 400);
+    };
 
     if (tasks.length === 0) {
         return (
@@ -17,19 +34,44 @@ export const TaskList = ({ tasks }: TasklistProps) => {
     }
 
     return (
-        <View className="flex items-center p-4 ">
+        <View className="flex items-center p-4 h-screen-safe">
             <FlatList
                 showsVerticalScrollIndicator={false}
                 data={tasks}
+                contentContainerStyle={{ paddingBottom: 515 }}
                 keyExtractor={(item) => item.id}
                 renderItem={({ item }) => (
-                    <View className="flex flex-row justify-between w-[327px] h-[64px] items-center  bg-[#333333] p-4 rounded-md mb-2 mt-4">
-                        <TouchableOpacity >
-                            <AntDesign name="checkcircleo" size={20} color="#4EA8DE" />
+                    <View className={`flex flex-row justify-between w-[327px] h-[78px] items-center  bg-[#333333] p-4 rounded-lg border-[0.5px] ${item.completed ? 'border-[#4EA8DE]' : 'border-[#808080]'}  mt-3`}>
+                        <TouchableOpacity onPress={() => onToggleTask(item.id)}>
+                            {item.completed ? (
+                                <AntDesign name="checkcircle" size={24} color="#4EA8DE" />
+                            ) : (
+                                <MaterialIcons name="radio-button-unchecked" size={24} color="#4EA8DE" />
+
+                            )}
+
                         </TouchableOpacity>
-                        <Text className="text-white">{item.title}</Text>
-                        <TouchableOpacity>
-                            <Feather name="trash-2" size={20} color="#808080" />
+
+                        <Text className={`flex mx-3 ${item.completed ? 'text-[#808080] line-through' : 'text-white'}`}>
+                            {item.title}
+                        </Text>
+
+                        <TouchableOpacity
+                            className={`w-[28px] h-[28px] rounded-lg flex items-center justify-center ${pressedId === item.id
+                                ? 'bg-[#808080]'
+                                : removingId === item.id
+                                    ? <Feather name="trash-2" size={20} color="#E25858" />
+                                    : ''
+                                }`}
+                            onPress={() => handleRemoveWithDelay(item.id)}
+                            onPressIn={() => setPressedId(item.id)}
+                            onPressOut={() => setPressedId(null)}
+                        >
+                            <Feather
+                                name="trash-2"
+                                size={20}
+                                color={pressedId === item.id || removingId === item.id ? "#E25858" : "#808080"}
+                            />
                         </TouchableOpacity>
                     </View>
                 )}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,12 +4,26 @@ import { Header } from 'src/components/Header';
 import { MainInput } from '~/components/MainInput';
 import { Separator } from '~/components/Separator';
 import { TaskCount } from '~/components/TaskCount';
-import { TaskEmpty } from '~/components/TaskEmpty';
 import { TaskList } from '~/components/TaskList';
 import { Task } from '~/types';
 
 export const HomeScreen = () => {
     const [tasks, setTasks] = useState<Task[]>([]);
+
+    const toggleTaskCompleted = (id: string) => {
+        setTasks((prevTasks) => {
+            return prevTasks.map((task) => {
+                return task.id === id ? { ...task, completed: !task.completed } : task
+            })
+        })
+
+    }
+
+    const removeTask = (id: string) => {
+        setTasks((prevTasks => prevTasks.filter(task => task.id !== id)))
+    }
+
+    const completedTasks = tasks.filter(tasks => tasks.completed).length
 
     return (
         <SafeAreaView className="w-full h-full flex-1 bg-[#1A1A1A] ">
@@ -17,11 +31,12 @@ export const HomeScreen = () => {
             <View className="w-full h-full ">
                 <Header />
                 <MainInput setTasks={setTasks} tasks={tasks} />
-                <TaskCount total={5} completed={3} />
+                <TaskCount total={tasks.length} completed={completedTasks} />
                 <Separator />
-                <TaskList tasks={tasks} />
+                <TaskList onToggleTask={toggleTaskCompleted} onRemoveTask={removeTask} tasks={tasks} />
             </View>
 
         </SafeAreaView>
-    );
+    )
+        ;
 }
